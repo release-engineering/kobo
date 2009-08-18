09cbcb6a177d363b82e8eb7518f83d3c96790bcd
Tomas Kopecek <tkopecek@redhat.com>
2009-08-18 16:31:29 +0200

    Read default paginate_by value from settings in kobo.django.views.generic.object_list.

--- kobo/django/views/generic.py
+++ kobo/django/views/generic.py
@@ -2,10 +2,15 @@
 
 
 import django.views.generic.list_detail
+from django.conf import settings
 
 
 def object_list(request, queryset, **kwargs):
-    """Appends GET variables to the context. Use for paginated searches."""
+    """
+    Appends GET variables to the context. Use for paginated searches.
+    In settings.py you can set optional parameter PAGINATE_BY which will be
+    used as a default value.
+    """
 
     kwargs.setdefault("extra_context", {})
 
@@ -17,5 +22,8 @@ def object_list(request, queryset, **kwargs):
         kwargs["extra_context"]["get_vars"] = "&%s" % get_vars.urlencode()
     else:
         kwargs["extra_context"]["get_vars"] = ""
+
+    if not "paginate_by" in kwargs:
+        kwargs["paginate_by"] = getattr(settings, "PAGINATE_BY", None)
 
     return django.views.generic.list_detail.object_list(request, queryset, **kwargs)
