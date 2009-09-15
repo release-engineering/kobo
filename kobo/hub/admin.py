5fb11da2e7b42036789a08b1cbea42b3bcd294be
Tomas Kopecek <tkopecek@redhat.com>
2009-09-15 11:39:25 +0200

    More convenient admin listing for Worker model.

--- kobo/hub/admin.py
+++ kobo/hub/admin.py
@@ -12,8 +12,10 @@ class TaskAdmin(admin.ModelAdmin):
     search_fields = ("id", "method", "label", "owner", "dt_created", "dt_finished")
     raw_id_fields = ("parent", "owner", "resubmitted_by", "resubmitted_from")
 
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ("name", "enabled", "ready", "max_load", "current_load", "task_count")
 
 admin.site.register(Arch)
 admin.site.register(Channel)
-admin.site.register(Worker)
+admin.site.register(Worker, WorkerAdmin)
 admin.site.register(Task, TaskAdmin)
