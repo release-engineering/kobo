4630b898e37d0653baa22c98578eb06c82eebfe6
Tomas Kopecek <tkopecek@redhat.com>
2009-09-23 16:16:53 +0200

    Fix TaskAdmin to search for user in correct db field.

--- kobo/hub/admin.py
+++ kobo/hub/admin.py
@@ -9,7 +9,7 @@ from models import *
 class TaskAdmin(admin.ModelAdmin):
     list_display = ("id", "method", "label", "state", "owner", "dt_created", "dt_finished", "time", "arch", "channel")
     list_filter = ("method", "state")
-    search_fields = ("id", "method", "label", "owner", "dt_created", "dt_finished")
+    search_fields = ("id", "method", "label", "owner__username", "dt_created", "dt_finished")
     raw_id_fields = ("parent", "owner", "resubmitted_by", "resubmitted_from")
 
 class WorkerAdmin(admin.ModelAdmin):
