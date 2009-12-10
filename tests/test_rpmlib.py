728f343db3ddebec35b2125cbb5919d29fbf4d6b
Tomas Kopecek <tkopecek@redhat.com>
2009-12-10 11:15:41 +0100

    Convert rpm.labelCompare's arguments to string to prevent segfaulting. Add compare_nvr tests. (cherry picked from commit c10ac42db99a8225a69b3220bc7b70d12f3e7743)

--- tests/test_rpmlib.py
+++ tests/test_rpmlib.py
@@ -32,6 +32,33 @@ class TestNVR(unittest.TestCase):
         self.assertEqual(parse_nvra("net-snmp-5.3.2.2-5.el5.i386.rpm:1"), dict(name="net-snmp", version="5.3.2.2", release="5.el5", epoch="1", arch="i386", src=False))
         self.assertEqual(parse_nvra("net-snmp-5.3.2.2-5.el5.i386:1.rpm"), dict(name="net-snmp", version="5.3.2.2", release="5.el5", epoch="1", arch="i386", src=False))
 
+    def test_compare_nvr(self):
+        first = {'name': 'a', 'version': '1', 'release': '1', 'epoch': '1'}
+        second = {'name': 'a', 'version': '1', 'release': '1', 'epoch': '1'}
+        self.assertEqual(compare_nvr(first, second), 0)
+        second['version'] = '0'
+        self.assertEqual(compare_nvr(first, second), 1)
+        second['version'] = 0
+        self.assertEqual(compare_nvr(first, second), 1)
+        second['version'] = 2
+        self.assertEqual(compare_nvr(first, second), -1)
+        second['version'] = 1
+        second['release'] = 0
+        self.assertEqual(compare_nvr(first, second), 1)
+        second['release'] = 2
+        self.assertEqual(compare_nvr(first, second), -1)
+        second['release'] = 1
+        second['epoch'] = 0
+        self.assertEqual(compare_nvr(first, second), 1)
+        second['epoch'] = 2
+        self.assertEqual(compare_nvr(first, second), -1)
+        first = {'name': 'a', 'version': '1', 'release': '1', 'epoch': None}
+        second = {'name': 'a', 'version': '1', 'release': '1', 'epoch': '1'}
+        self.assertEqual(compare_nvr(first, second), -1)
+        # missing epoch
+        first = {'name': 'a', 'version': '1', 'release': '1'}
+        self.assertEqual(compare_nvr(first, second), -1)
+
 
 if __name__ == '__main__':
     unittest.main()
