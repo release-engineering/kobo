c0c822df243894106a7fd376582598e4aebb4c24
Daniel Mach <dmach@redhat.com>
2010-01-28 19:04:06 +0100

    Check worker's FQDN against username.

--- kobo/hub/decorators.py
+++ kobo/hub/decorators.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+import socket
+
 from django.core.exceptions import PermissionDenied, SuspiciousOperation
 from kobo.decorators import decorator_with_args
 from kobo.django.xmlrpc.decorators import *
@@ -13,6 +15,11 @@ def validate_worker(func):
 
         if getattr(request, 'worker', None) is None:
             raise SuspiciousOperation("User doesn't match any worker: %s" % request.user.username)
+
+        fqdn = socket.getfqdn(request.META["REMOTE_ADDR"])
+        prefix, hostname = request.user.username.split("/", 1)
+        if hostname != fqdn:
+            raise SuspiciousOperation("Worker's FQDN (%s) doesn't match username (%s)" % (fqdn, hostname))
 
         return func(request, *args, **kwargs)
 
