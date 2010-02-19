55ab3056651496a721185d9b7b8886f908b6008f
Daniel Mach <dmach@redhat.com>
2010-02-19 11:29:50 +0100

    Javascript log watcher.

--- kobo/hub/views.py
+++ kobo/hub/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
+import os
+
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.generic.list_detail import object_detail
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 
 from kobo.django.views.generic import object_list
 from kobo.hub.models import Task, Worker, Channel, Arch
@@ -147,8 +151,19 @@ def task_list(request, state):
 
 def task_detail(request, id):
     task = get_object_or_404(Task, id=id)
+
+    logs = []
+    for i in task.logs.list:
+        if request.user.is_superuser:
+            logs.append(i)
+            continue
+        if not os.path.basename(i).startswith("traceback"):
+            logs.append(i)
+    logs.sort(lambda x, y: cmp(os.path.split(x), os.path.split(y)))
+
     c = {
 	"task_list": task.subtasks(),
+	"logs": logs,
     }
 
     dict = {
@@ -160,3 +175,49 @@ def task_detail(request, id):
     }
 
     return object_detail(request, **dict)
+
+
+def task_log(request, id, log_name):
+    """
+    IMPORTANT: reverse to 'task/log-json' *must* exist
+    """
+    if os.path.basename(log_name).startswith("traceback") and not request.user.is_superuser:
+        return HttpResponseForbidden()
+
+    task = get_object_or_404(Task, id=id)
+    offset = int(request.GET.get("offset", 0))
+    content = task.logs[log_name][offset:]
+
+    if request.GET.get("format") == "txt":
+        response = HttpResponse(mimetype='text/plain')
+        response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(log_name)
+        response.write(content)
+        return response
+
+    context = {
+        "offset": offset + len(content) + 1,
+        "task_finished": task.is_finished() and 1 or 0,
+        "content": content,
+        "log_name": log_name,
+        "task": task,
+        "json_url": reverse("task/log-json", args=[id, log_name]),
+    }
+
+    return render_to_response("task/log.html", context, context_instance=RequestContext(request))
+
+
+def task_log_json(request, id, log_name):
+    if os.path.basename(log_name).startswith("traceback") and not request.user.is_superuser:
+        return HttpResponseForbidden(mimetype="application/json")
+
+    task = get_object_or_404(Task, id=id)
+    offset = int(request.GET.get("offset", 0))
+    content = task.logs[log_name][offset:]
+
+    result = {
+        "new_offset": offset + len(content),
+        "task_finished": task.is_finished() and 1 or 0,
+        "content": content,
+    }
+
+    return HttpResponse(simplejson.dumps(result), mimetype="application/json")
