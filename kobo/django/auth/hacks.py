4abcbd9ebcb005c8c1e95b35d18543e2e5400aab
Martin Bukatovic <mbukatov@redhat.com>
2010-11-24 15:13:22 +0100

    Fix username max_length hack to work with Django 1.2.1.

--- kobo/django/auth/hacks.py
+++ kobo/django/auth/hacks.py
@@ -5,21 +5,26 @@ import django.forms
 import django.contrib.auth.forms
 import django.contrib.auth.models
 from django.utils.translation import ugettext_lazy as _
+from django.core import validators
 
 
 # extend username max_length to 255
-django.contrib.auth.models.User._meta.get_field_by_name("username")[0].max_length = 255
-
+MAX_LENGTH = 255
+username_field = django.contrib.auth.models.User._meta.get_field_by_name("username")[0]
+username_field.max_length = MAX_LENGTH
+username_field.validators = []
+username_field.validators.append(validators.MaxLengthValidator(MAX_LENGTH))
+del username_field
 
 user_re = r"^[\/\-\.\w]+$"
-help_text = _("Required. 255 characters or fewer. Alphanumeric characters only (letters, digits, underscores and slashes).")
+help_text = _("Required. %s characters or fewer. Alphanumeric characters only (letters, digits, underscores and slashes)." % MAX_LENGTH)
 error_message = _("This value must contain only letters, numbers, underscores and slashes.")
 
 
 class UserCreationForm(django.contrib.auth.forms.UserCreationForm):
     username = django.forms.RegexField(
         label=_("Username"),
-        max_length=255,
+        max_length=MAX_LENGTH,
         regex=user_re,
         help_text = help_text,
         error_message = error_message,
@@ -36,7 +41,7 @@ class UserCreationForm(django.contrib.auth.forms.UserCreationForm):
 class UserChangeForm(django.contrib.auth.forms.UserChangeForm):
     username = django.forms.RegexField(
         label=_("Username"),
-        max_length=255,
+        max_length=MAX_LENGTH,
         regex=user_re,
         help_text = help_text,
         error_message = error_message,
