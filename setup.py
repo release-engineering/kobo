376dd2255a273eda1f1a883465aea14f7d851bc6
Daniel Mach <dmach@redhat.com>
2011-10-10 11:46:47 +0200

    Bump version to 0.3.5.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ project_author_email = "dmach@redhat.com"
 project_description  = "Python modules for tools development"
 package_name         = "%s" % project_name
 package_module_name  = project_name
-package_version      = [0, 3, 4, "final", ""]
+package_version      = [0, 3, 5, "final", ""]
 
 
 script_files = []
