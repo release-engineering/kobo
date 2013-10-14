e887f63a5cf89095252a88031f66031c55a5fe6a
Daniel Mach <dmach@redhat.com>
2013-10-14 10:01:22 +0200

    Bump version to 0.4.1.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ for package in package_data.keys():
 
 setup(
     name            = "kobo",
-    version         = "0.4.0",
+    version         = "0.4.1",
     description     = "Python modules for tools development",
     url             = "https://fedorahosted.org/kobo/",
     author          = "Red Hat, Inc.",
