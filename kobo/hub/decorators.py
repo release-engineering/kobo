6d83e409dcf56806b433c10012717b3551c69a4f
Rayson Zhu <vfreex+git@gmail.com>
2016-10-21 22:50:26 +0800

    Remove reverse DNS validation for Kobo worker

--- kobo/hub/decorators.py
+++ kobo/hub/decorators.py
@@ -16,11 +16,6 @@ def validate_worker(func):
         if getattr(request, 'worker', None) is None:
             raise SuspiciousOperation("User doesn't match any worker: %s" % request.user.username)
 
-        fqdn = socket.getfqdn(request.META["REMOTE_ADDR"])
-        prefix, hostname = request.user.username.split("/", 1)
-        if hostname != fqdn:
-            raise SuspiciousOperation("Worker's FQDN (%s) doesn't match username (%s)" % (fqdn, hostname))
-
         return func(request, *args, **kwargs)
 
     _new_func.__name__ = func.__name__
