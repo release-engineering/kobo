df2ec97a412b042404c04211c22a5d058435bfa0
Lumir Balhar <lbalhar@redhat.com>
2017-08-30 14:39:00 +0200

    python3: Fix str/bytes issues.
    For some reason get_traceback() returns string but Tracebak.get_traceback()
    returns bytes so we need regex for both of them.

--- tests/test_tback.py
+++ tests/test_tback.py
@@ -20,28 +20,30 @@ class TestTraceback(unittest.TestCase):
         try:
             raise Exception('Simple text')
         except:
-            regexp = re.compile('Traceback \(most recent call last\):\n *File ".*test_tback.py", line .+, in test_text\n *raise Exception\(\'Simple text\'\)\n *Exception: Simple text', re.M)
-            self.assertRegexpMatches(get_traceback(), regexp)
+            str_regexp = re.compile('Traceback \(most recent call last\):\n *File ".*test_tback.py", line .+, in test_text\n *raise Exception\(\'Simple text\'\)\n *Exception: Simple text', re.M)
+            bytes_regexp = re.compile(b'Traceback \(most recent call last\):\n *File ".*test_tback.py", line .+, in test_text\n *raise Exception\(\'Simple text\'\)\n *Exception: Simple text', re.M)
+
+            self.assertRegex(get_traceback(), str_regexp)
             tb = Traceback(show_traceback = True, show_code = False, show_locals = False, show_environ = False, show_modules = False)
-            self.assertRegexpMatches(tb.get_traceback(), regexp)
+            self.assertRegex(tb.get_traceback(), bytes_regexp)
 
     def test_Traceback(self):
         try:
             raise Exception('Simple text')
         except:
             tb = Traceback(show_traceback = False, show_code = False, show_locals = False, show_environ = False, show_modules = False)
-        self.assertEqual('', tb.get_traceback())
+        self.assertEqual(b'', tb.get_traceback())
         tb.show_code = True
-        self.assertRegexpMatches(tb.get_traceback(), re.compile('<CODE>.*--> *\d+ *raise Exception.*<\/CODE>$', re.M | re.S))
+        self.assertRegex(tb.get_traceback(), re.compile(b'<CODE>.*--> *\d+ *raise Exception.*<\/CODE>$', re.M | re.S))
         tb.show_code = False
         tb.show_locals = True
-        self.assertRegexpMatches(tb.get_traceback(), re.compile('<LOCALS>.*tb = .*<\/LOCALS>$', re.M | re.S))
+        self.assertRegex(tb.get_traceback(), re.compile(b'<LOCALS>.*tb = .*<\/LOCALS>$', re.M | re.S))
         tb.show_locals = False
         tb.show_environ = True
-        self.assertRegexpMatches(tb.get_traceback(), re.compile('<ENVIRON>.*<\/ENVIRON>\n<GLOBALS>.*</GLOBALS>$', re.M | re.S))
+        self.assertRegex(tb.get_traceback(), re.compile(b'<ENVIRON>.*<\/ENVIRON>\n<GLOBALS>.*</GLOBALS>$', re.M | re.S))
         tb.show_environ = False
         tb.show_modules = True
-        self.assertRegexpMatches(tb.get_traceback(), re.compile('<MODULES>.*<\/MODULES>$', re.M | re.S))
+        self.assertRegex(tb.get_traceback(), re.compile(b'<MODULES>.*<\/MODULES>$', re.M | re.S))
 
     def test_encoding(self):
         try:
