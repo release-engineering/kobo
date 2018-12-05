a2c942911ae845c6962d21d6a0113ef88f69c228
Alexandre Vicenzi <avicenzi@redhat.com>
2018-12-05 13:49:10 +0100

    Test XMLRPC auth

---
+++ tests/test_xmlrpc_auth.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import django
+
+# Only for Django >= 1.7
+if 'setup' in dir(django):
+    # This has to happen before below imports because they have a hard requirement
+    # on settings being loaded before import.
+    django.setup()
+
+from django.core.exceptions import PermissionDenied
+from mock import Mock, patch
+
+from kobo.hub.models import Worker
+from kobo.hub.xmlrpc import auth
+
+from .utils import DjangoRunner
+
+runner = DjangoRunner()
+setup_module = runner.start
+teardown_module = runner.stop
+
+
+class TestLoginWorker(django.test.TestCase):
+
+    def test_login_worker_key_valid_worker_and_user(self):
+        def login(request, user):
+            request.session.session_key = '1234567890'
+            return user
+
+        Worker.objects.create(worker_key='key', name='name')
+
+        req = Mock(spec=['session'], session=Mock())
+        user = Mock()
+        krb_mock = Mock(spec=['authenticate'], authenticate=Mock(return_value=user))
+
+        with patch('kobo.hub.xmlrpc.auth.Krb5RemoteUserBackend', return_value=krb_mock):
+            with patch.object(auth.django.contrib.auth, 'login', side_effect=login) as login_mock:
+                session_key = auth.login_worker_key(req, 'key')
+
+                login_mock.assert_called_once_with(req, user)
+
+        krb_mock.authenticate.assert_called_once_with('worker/name')
+        self.assertEquals(session_key, '1234567890')
+
+    def test_login_worker_key_valid_worker_invalid_user(self):
+        Worker.objects.create(worker_key='key', name='name')
+        req = Mock(spec=['session'], session=Mock())
+        krb_mock = Mock(spec=['authenticate'], authenticate=Mock(return_value=None))
+
+        with patch('kobo.hub.xmlrpc.auth.Krb5RemoteUserBackend', return_value=krb_mock):
+            with self.assertRaises(PermissionDenied):
+                auth.login_worker_key(req, 'key')
+
+        krb_mock.authenticate.assert_called_once_with('worker/name')
+
+    def test_login_worker_key_invalid_worker(self):
+        req = Mock()
+
+        with self.assertRaises(PermissionDenied):
+            auth.login_worker_key(req, 'key')
