8fb6015d0095f26d940f7d09e58f13041fc52f0a
Matej Mužila <mmuzila@redhat.com>
2019-11-07 14:35:27 +0100

    Modernize login view
    Use of function based django.contrib.auth.views.login view was deprecated in
    django 1.11 [1]. Add support for more modern class-based
    django.contrib.auth.views.LoginView [2].
    
    [1] https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.login
    [2] https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.LoginView

--- kobo/hub/urls/auth.py
+++ kobo/hub/urls/auth.py
@@ -3,10 +3,18 @@
 
 from django.conf.urls import url
 import kobo.hub.views
+from kobo.django.django_version import django_version_ge
 
+if django_version_ge('1.11.0'):
+    urlpatterns = [
+        url(r"^login/$", kobo.hub.views.LoginView.as_view(), name="auth/login"),
+        url(r"^krb5login/$", kobo.hub.views.krb5login, name="auth/krb5login"),
+        url(r'^logout/$', kobo.hub.views.logout, name="auth/logout"),
+    ]
 
-urlpatterns = [
-    url(r"^login/$", kobo.hub.views.login, name="auth/login"),
-    url(r"^krb5login/$", kobo.hub.views.krb5login, name="auth/krb5login"),
-    url(r'^logout/$', kobo.hub.views.logout, name="auth/logout"),
-]
+else:
+    urlpatterns = [
+        url(r"^login/$", kobo.hub.views.login, name="auth/login"),
+        url(r"^krb5login/$", kobo.hub.views.krb5login, name="auth/krb5login"),
+        url(r'^logout/$', kobo.hub.views.logout, name="auth/logout"),
+    ]
