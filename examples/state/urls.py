78dfc08349c91fe3d73166a2ce574f325f6401b8
Lubomir Gallovic <lgallovi@redhat.com>
2020-06-23 15:12:03 +0200

    Change admin.site.urls to use a direct path
    The change[1] should be compatible with older Django versions as well.
    
    [1] https://docs.djangoproject.com/en/1.11/releases/1.9/#passing-a-3-tuple-or-an-app-name-to-include

--- examples/state/urls.py
+++ examples/state/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', admin.site.urls),
     (r"(?P<id>\d+)", "state.app.views.form_view"),
     (r"", "state.app.views.form_view"),
 )
