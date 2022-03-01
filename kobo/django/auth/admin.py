0fa1af05d13725c680da2cd40f9f87262ca3172e
Ľubomír Gallovič <lgallovi@redhat.com>
2022-03-01 14:24:56 +0100

    Add custom user to the admin page
    As kobo has its own user implementation, its model doesn't get added to
    the admin page automatically. Register the user model so that it can be
    modified via the admin page. This only affects newer versions of Django.

--- kobo/django/auth/admin.py
+++ kobo/django/auth/admin.py
@@ -1,1 +1,9 @@
 from django.contrib.auth.admin import *
+import django.contrib.admin as admin
+from kobo.django.django_version import django_version_ge
+
+from kobo.django.auth.models import *
+
+# users are not displayed on admin page since migrations were introduced
+if django_version_ge("1.9.0"):
+    admin.site.register(User, UserAdmin)
